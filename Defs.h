d0a2adeae265e9197cc7564db8fbcfefc4072a2b
Ravi <ravi.genius@gmail.com>
2013-04-11 15:54:33 -0400

    Added code and deleted Error.h

---
+++ Defs.h
@@ -0,0 +1,16 @@
+
+#define ERR_ALREADY_EXISTS "Cannot create network; node is already connected!\n"
+
+#define NUMBER_OF_SUCCESSORS 5
+
+#define ERR_NODE_IP_NOT_SET "Node URL is not set yet!"
+
+#define ERR_NODE_ID_NOT_SET "Node ID is not set yet!"
+
+#define ERR_IP_INVALID "IP address is invalid!"
+
+#define FILE_NOT_FOUND "Error: File not found"
+
+#define WRONG_NUM_SUCCESSORS "NUMBER_OF_SUCCESSORS intialized with wrong value!"
+
+#define ERR_NODE_NULL "Error: Node is null"
